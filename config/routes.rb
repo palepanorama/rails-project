186d71b3cbfdab67958a16e8a3a63749a1df9e35
palepanorama <matthew.keith.thomas@gmail.com>
2020-12-01 15:12:48 -0600

    updates routes to use callbacks controller and adds scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
   root to: 'application#welcome'
+
+  devise_scope :user do 
+    get 'login', to: 'devise/sessions#new', as: 'login'
+    get 'signup', to: 'devise/registrations', as: 'signup'
+  end 
 end
