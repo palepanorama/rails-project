485453953f99b321abf7f26088c1565db7fc64a8
palepanorama <matthew.keith.thomas@gmail.com>
2020-12-04 11:16:14 -0600

    adds selected method to players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,30 @@
 class PlayersController < ApplicationController
+
+    def index
+    end 
+
+    def show 
+    end 
+
+    def new
+    end 
+
+    def edit
+    end 
+
+    def create
+    end 
+
+    def update
+    end
+
+    def destroy
+    end 
+
+    def selected
+        @players = Player.selected 
+    end
+
+    private 
+
 end
