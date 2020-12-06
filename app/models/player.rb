58a09a00c8847b2ab9570f9036a115e0cdda6831
palepanorama <matthew.keith.thomas@gmail.com>
2020-12-06 16:13:52 -0600

    updates partial players view to use dropbox for positions

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,7 @@
 class Player < ApplicationRecord
     belongs_to :user 
     belongs_to :position 
-    validates :name, presence: true 
-    # accepts_nested_attributes_for :positions 
+    validates :name, presence: true  
 
     scope :selected, -> {where(has_team: true)}
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,7 +1,6 @@
 class Position < ApplicationRecord
     has_many :players 
     has_many :users, through: :players 
-    validates :name, presence: true 
 
     accepts_nested_attributes_for :players 
 end
