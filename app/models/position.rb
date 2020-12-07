66985e7fbf66a664642f6549db4abbc814b3d517
palepanorama <matthew.keith.thomas@gmail.com>
2020-12-06 19:30:11 -0600

    adds validations to position model

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,7 @@
 class Position < ApplicationRecord
     has_many :players 
     has_many :users, through: :players 
+    validates :name, uniqueness: true 
 
     accepts_nested_attributes_for :players 
 end
