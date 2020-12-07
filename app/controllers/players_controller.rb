35326657d7aad2ac179d506e3c3465d5acefde52
palepanorama <matthew.keith.thomas@gmail.com>
2020-12-06 18:11:13 -0600

    updates edit method in players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,11 +1,8 @@
 class PlayersController < ApplicationController
 
     def index
-        @players = Player.all.selected 
-    end 
-
-    def show 
-        @player = Player.find(params[:id])
+        # @players = Player.all.selected 
+        @players = current_user.players.selected 
     end 
 
     def new
@@ -15,6 +12,23 @@ class PlayersController < ApplicationController
     def create
         @player = current_user.players.create(player_params)
         redirect_to player_path(@player)
+    end 
+
+    def show 
+        @player = Player.find(params[:id])
+    end 
+
+    def edit 
+        @player = Player.find_by(id: params[:id])
+    end 
+
+    def update 
+        @player = Player.find_by(id: params[:id])
+    end 
+
+    def destroy 
+        Player.find(params[:id]).destroy 
+        redirect_to root_path 
     end 
 
     def selected
